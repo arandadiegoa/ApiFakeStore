23c95474ab2315cd81e4b6269bd96ba4b3f5ba96
arandadiegoa <arandadiegoa@gmail.com>
2022-09-27 20:37:46 -0300

    Armado de Proyecto

---
+++ src/main.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Pages/Home";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Product from "./Pages/Product";
+import Category from "./Pages/Category";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/product/:id",
+    element: (
+      <div>
+        <Product />
+      </div>
+    )
+  },
+  {
+    path: "/category/:name",
+    element: (
+      <div>
+        <Category />
+      </div>
+    )
+  },
+  {
+    path: "/login",
+    element: (
+      <div>
+        <Login />
+      </div>
+    )
+  },
+  {
+    path: "/cart",
+    element: (
+      <div>
+        <Cart />
+      </div>
+    )
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <RouterProvider router={router} />
+  </div>
+);
